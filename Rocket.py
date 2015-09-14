c6bae8e3e9f7beb3bbf0ef6e0d8aba295b6f75cf
97gushan <97gustavh@gmail.com>
2015-09-14 09:44:04 +0200

    Rocket file
    This file contains the rocket class

---
+++ Rocket.py
@@ -0,0 +1,67 @@
+from kivy.graphics import *
+
+
+class Rocket:
+    def __init__(self, xpos, ypos, direc):
+        self.xpos = xpos
+        self.start_x = xpos
+        self.ypos = ypos
+        self.speed = 350
+        self.max_speed = 1000
+        
+        self.direc = direc
+        
+    def draw(self, widget, window_x, window_y, sq_w, sq_h):
+        """ draws the rocket"""
+        Color(1.,0,0)        
+        
+        if(self.direc == 1 or self.direc == 2):
+            Rectangle(pos=(80 + window_x + self.xpos, 20 + window_y + self.ypos * sq_h),\
+                      size=(sq_w - 30, sq_h - 30))
+        elif(self.direc == 3 or self.direc == 4):
+            Rectangle(pos=( 20 + window_x + self.xpos * sq_w, 80 + window_y + self.ypos),\
+                      size=(sq_w-30, sq_h-30))
+    
+    def move(self, dt):
+        if(self.direc == 1):
+            self.xpos -= self.speed *dt
+        elif(self.direc == 2):
+            self.xpos += self.speed *dt
+        elif(self.direc == 3):
+            self.ypos -= self.speed *dt
+        elif(self.direc == 4):
+            self.ypos += self.speed *dt
+        
+    def get_x(self):
+        """ return the xpos"""
+        return self.xpos
+    
+    def get_y(self):
+        """ return the ypos"""
+        return self.ypos
+        
+    def get_direc(self):
+        
+        return self.direc
+    
+    def change_speed(self):
+        """
+        if(self.speed * 1.1 < self.max_speed):        
+            self.speed *= 1.1
+        else:
+            self.speed = self.max_speed
+            """
+        self.speed = 1000
+    def set_start_x(self):
+        self.xpos = self.start_x
+    
+    def set_direc(self, direc):
+        self.direc = direc
+        
+    def set_x(self, xpos):
+        """ set the xpos"""
+        self.xpos = xpos
+        
+    def set_y(self, ypos): 
+        """ set the ypos"""
+        self.ypos = ypos
