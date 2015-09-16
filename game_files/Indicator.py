79643f8ac76e95fcf79e9ecdd28dbd8dbd9835e8
97gushan <97gustavh@gmail.com>
2015-09-16 11:52:45 +0200

    minor changes to indicator class
    renamed a couple of variables and gave the function set_new_pos a
    parameter

--- game_files/Indicator.py
+++ game_files/Indicator.py
@@ -4,12 +4,12 @@ from kivy.graphics import *
 class Indicator:
     """ This is the indicator class"""
     
-    def __init__(self, xpos, ypos, direc, bord_w, bord_h):
+    def __init__(self, xpos, ypos, direc, board_w, board_h):
         self.xpos = xpos
         self.ypos = ypos
         self.direc = direc
-        self.bord_w = bord_w
-        self.bord_h = bord_h        
+        self.board_w = board_w
+        self.board_h = board_h        
         
     def draw(self, widget, window_x, window_y, sq_w, sq_h):
         """ This function controlls the drawing of the indicator"""
@@ -18,16 +18,16 @@ class Indicator:
                        10 + window_y + sq_h * self.ypos),\
                   size=(sq_w, sq_h))
                   
-    def set_new_pos(self):
+    def set_new_pos(self, direc):
         """ This function gives the indicator a new position depending on wich 
         direction that the rocket are coming from"""
-        if(self.direc == 1):
-            self.xpos = self.bord_w
-        elif(self.direc == 2):
+        if(direc == 1):
+            self.xpos = self.board_w
+        elif(direc == 2):
             self.xpos = -1
-        elif(self.direc == 3):
-            self.ypos = self.bord_h
-        elif(self.direc == 4):
+        elif(direc == 3):
+            self.ypos = self.board_h
+        elif(direc == 4):
             self.ypos = -1
     
     def get_x(self):
@@ -48,5 +48,5 @@ class Indicator:
         
     def set_border_size(self, x, y):
         """ Updates the border size"""
-        self.bord_w = x
-        self.bord_h = y
+        self.board_w = x
+        self.board_h = y
