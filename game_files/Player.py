545fc70313b09f79d3f13005557d22f2796fba17
97marcar <hans.hansson22@gmail.com>
2015-09-16 15:00:25 +0200

    Added image for player
    added so the player changes between 2 pictures every second

--- game_files/Player.py
+++ game_files/Player.py
@@ -8,20 +8,30 @@ class Player:
         # sets the position of the player        
         self.xpos = xpos
         self.ypos = ypos
-        
+        self.image = ["prealphacube80x80.png", "prealphacube80x802.png"]
+        self.current_image = "prealphacube80x80.png"
+        self.num = 0
         self.board_width = board_width
         self.board_height = board_height
-        
+        self.value_for_image = False
         # these are not used right now
         self.size_x = size_x
         self.size_y = size_y
-
+        
+        
+    def update_image(self, function):
+      if float(function) % 1.0 != 0:
+        self.value_for_image = True
+      if float(function) % 1.0 == 0 and self.value_for_image:
+        self.num += 1
+        if self.num == (len(self.image)):
+          self.num = 0
+        self.value_for_image = False
+      self.current_image = self.image[self.num]
         
     def draw(self, widget, window_x, window_y, sq_w, sq_h):
         """ draw out the player"""
-        Rectangle(pos=(10+window_x + sq_w * self.xpos, 
-                       10 + window_y + sq_h * self.ypos),\
-                  size=(sq_w, sq_h))
+        Rectangle(source =self.current_image, pos=(10+window_x + sq_w * self.xpos, 10 + window_y + sq_h * self.ypos), size=(sq_w-10, sq_h-10))
 
         
     def move_up(self, direc):
