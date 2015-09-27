e3326d15141bfd2842c5e3c2d156e82363fb689c
97marcar <hans.hansson22@gmail.com>
2015-09-27 15:20:44 +0200

    Added new pausebutton
    Added the new pausebutton i made (still need some resizing and
    positionen to do.)

--- game_files/main.py
+++ game_files/main.py
@@ -120,9 +120,12 @@ class RootWidget(BoxLayout):
             self.rocket_control.draw(self.play_field_widget, self.play_field_x, self.play_field_y, self.sq_w, self.sq_h)
 
             #Timerlabel
-
             Label(text=self.timer_text, pos=(self.play_field_width*0.01,self.play_field_height * 1.025))
             
+            #Pausebutton
+            Rectangle(source="img/pausebutton.png", pos=(self.play_field_width*1,self.play_field_height * 1.1), size=(self.sq_w*0.19, self.sq_h*0.19))
+
+            #Deathscreen            
             if self.rip:
               Rectangle(source="img/ripscreen.png", pos=(self.play_field_x+0.5*self.sq_w, \
               self.play_field_y+self.sq_h), size=(self.sq_w*2, self.sq_h*3))
