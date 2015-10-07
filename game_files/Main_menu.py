5d4a205bb442590068049d03015443fed96741a6
97gushan <97gustavh@gmail.com>
2015-10-07 16:06:42 +0200

    Added touch_down function and draw out temporary buttons
    I've added a function that is called when the menu is on that takes the
    touch-position as an argument. It going to check where the user touches
    the screen and if it is on a button
    
    I've printed out 3 temporary buttons and a temporary logo

--- game_files/Main_menu.py
+++ game_files/Main_menu.py
@@ -9,9 +9,33 @@ class Main_menu:
         
         
     def draw(self, widget, width, height):
-        Rectangle(pos=(100,100),size=(100,100))
+                
+        # title image
+        Rectangle(pos=(width//20,15 * height//20),\
+                  size=(width,height - 14 * height //20))
+        
+        # start game button
+        Rectangle(pos=(4* width//20, 11 * height//20),\
+                  size=(15*width // 20, 3* height //20))
+                  
+        # Highscore button
+        Rectangle(pos=(4 * width//20, 6 * height//20),\
+                  size=(15*width // 20, 3*height//20))
+        
+        # options game button
+        Rectangle(pos=(4* width//20, 1 * height//20), \
+                  size=(15*width // 20, 3 * height //20))
+    
+    def touch_down(self, touch):
+        """ this method is called when the player touches the screen while in
+        the main menu. It takes the touch-position as a argument and checks
+        if the touch is on a button"""
+        
+        xpos = touch.x
+        ypos = touch.y
         
         
+    
     
     def set_status(self, status):
         self._status = status
