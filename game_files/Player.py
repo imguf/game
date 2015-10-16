b553bd6747d17ba4398494430cfe755525d4c782
97marcar <hans.hansson22@gmail.com>
2015-10-16 12:16:24 +0200

    Added the first completed player image

--- game_files/Player.py
+++ game_files/Player.py
@@ -8,8 +8,8 @@ class Player:
         # sets the position of the player        
         self.xpos = xpos
         self.ypos = ypos
-        self.image = ["img/prealphacube80x80.png", "img/prealphacube80x802.png"]
-        self.current_image = "img/prealphacube80x80.png"
+        self.image = ["img/playerrocketv1.5.png", "img/playerrocketv1.5.png"]
+        self.current_image = "img/playerrocketv1.5.png"
         self.num = 0
         self.board_width = board_width
         self.board_height = board_height
