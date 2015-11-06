a80b3d2263cf83cf809b2d158b33a595649971c8
97gushan <97gustavh@gmail.com>
2015-11-06 12:02:54 +0100

    added get and set methods for self._active
    I've added a get and set method for the active boolean

--- game_files/highscore_window.py
+++ game_files/highscore_window.py
@@ -17,3 +17,13 @@ class Highscore_window:
         """ this method draws out the window on the screen"""
         Rectangle(source=(""), pos=(width//20, height//20),\
                 size=(19*width//20, 19*height//20))
+                
+    
+    def set_active(self, active):
+        """ this method takes a boolean as an arguments
+            it then sets the variable _active to that value"""
+        self._active = active
+        
+    def get_active(self):
+        """ this method returns the value of _active"""
+        return self._active
