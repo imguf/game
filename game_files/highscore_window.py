5167e90a9e6711b4c2b9a607c3810a08b799946d
97gushan <97gustavh@gmail.com>
2015-11-06 12:44:16 +0100

    Created highscore list and added get_save method
    I've created a list that will hold the highscore values.
    
    I've also added the get_save method that when called opens the save.txt
    file and sets the values of highscore to the values in the file

--- game_files/highscore_window.py
+++ game_files/highscore_window.py
@@ -12,12 +12,28 @@ class Highscore_window:
         
         self._active = False
         
+        # create highscore list
+        self.highscore = [0,0,0,0,0,0,0,0,0,0]
+        
         
     def draw(self, widget, width, height):
         """ this method draws out the window on the screen"""
         Rectangle(source=(""), pos=(width//20, height//20),\
                 size=(19*width//20, 19*height//20))
+    
+
+    def get_save(self):
+        try:        
+            file = open("./save.txt", "r")
+        except:
+            print("Wrong when opening file.")
+        else:
+            n = 0
+            for line in file:
+                self.highscore[n] = float(line)
+                n += 1
                 
+            file.close()
     
     def set_active(self, active):
         """ this method takes a boolean as an arguments
