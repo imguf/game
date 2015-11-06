3276cc21b2afa297290530c03f59c4470a4f413f
97gushan <97gustavh@gmail.com>
2015-11-06 13:30:19 +0100

    Draws out a temporary list layout
    I've added a temporary image for number 1 and it is printed out 10 times
    as a temporary layout for the highscore list

--- game_files/highscore_window.py
+++ game_files/highscore_window.py
@@ -1,4 +1,6 @@
 from kivy.graphics import *
+from kivy.uix.label import Label
+
 
 
 class Highscore_window:
@@ -15,13 +17,29 @@ class Highscore_window:
         # create highscore list
         self.highscore = [0,0,0,0,0,0,0,0,0,0]
         
+        self.number_images = ["img/test numbers/one.png"]
+        self.highscore_label_list = []
+        
         
     def draw(self, widget, width, height):
         """ this method draws out the window on the screen"""
+        Color(1,1,1)
         Rectangle(source=(""), pos=(width//20, height//20),\
-                size=(19*width//20, 19*height//20))
+                size=(20*width//20, 20*height//20))
+                
+        
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*1), size=(16,16))
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*2), size=(16,16))
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*3), size=(16,16))
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*4), size=(16,16))
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*5), size=(16,16))
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*6), size=(16,16))
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*7), size=(16,16))
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*8), size=(16,16))
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*9), size=(16,16))
+        Rectangle(source=(self.number_images[0]), pos=(100, 50*10), size=(16,16))
+        
     
-
     def get_save(self):
         try:        
             file = open("./save.txt", "r")
